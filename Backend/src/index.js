1b880993ab2de63b15df25a48c00f3affdff80d7
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2023-09-01 14:22:15 +0530

    Check now , cors open for all , one check route present

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require("express");
 const mongoose = require("mongoose")
-const path = require('path')
 const app = express();
 const cors = require("cors");
 const cookieParser = require('cookie-parser')
@@ -9,32 +8,55 @@ app.use(cookieParser()); // import the CORS middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //
-app.use(cors(
-  {
-    origin: ["https://tejuu-interview-hub.onrender.com"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-))
+// app.use(cors(
+//   {
+//     origin: ["*"],
+//     methods: ["POST", "GET"],
+//     credentials: true
+//   }
+// ))
 
-app.get("https://hosting-repo.vercel.app/load", (req, res) => {
-  res.send("hello")
-})
+// // // Cors for all frontEnds
+app.use(cors())
+
+// app.get("https://hosting-repo.vercel.app/load", (req, res) => {
+//   res.send("hello")
+// })
 
 
 //I changed here for Git check
-const controller = require("./controller/controller");
-const Middleware = require("./middleware/midW");
+// const controller = require("./controller/controller");
+// const Middleware = require("./middleware/midW");
 //route handler⤵️ 
 
-//----------------------------------------------------
-app.post("/login", Middleware.authJWT, controller.getUser);
-app.post("/register", controller.createUser1);
-app.get("/about", Middleware.authenticate, controller.requestSender);
-// app.get("/fetchName",controller.getUserbyQuery)
-// app.get("/companyAccess", controller.getCompany  )
-// --------------------------------------------------
-//  
+
+
+
+// -----------------------------
+// // For checking only 
+
+// app.use("/", (req, res) => {
+//   res.send("Hello Ak")
+// })
+
+
+
+// // // create router and check now
+
+const routes = require("./routes/routes")
+
+app.use( "/" , routes)
+
+
+
+// //----------------------------------------------------
+// app.post("/register", controller.createUser1);
+// app.post("/login", Middleware.authJWT, controller.getUser);
+// app.get("/about", Middleware.authenticate, controller.requestSender);
+// // app.get("/fetchName",controller.getUserbyQuery)
+// // app.get("/companyAccess", controller.getCompany  )
+// // --------------------------------------------------
+// //  
 
 
 mongoose.set('strictQuery', true)
--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -1,54 +1,26 @@
-require('dotenv').config()
-const express = require("express");
-const mongoose = require("mongoose")
-const path = require('path')
-const app = express();
-const cors = require("cors");
-const cookieParser = require('cookie-parser')
-app.use(cookieParser()); // import the CORS middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-//
-app.use(cors(
-  {
-    origin: ["https://tejuu-interview-hub.onrender.com"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-))
 
-app.get("https://hosting-repo.vercel.app/load", (req, res) => {
-  res.send("hello")
-})
+const router = require("express").Router()
 
 
 //I changed here for Git check
 const controller = require("../controller/controller");
 const Middleware = require("../middleware/midW");
-//route handler⤵️ 
+
+// // // For checking only -------->
+router.get("/c" , (req , res)=>{
+  res.send("Check now")
+})
+
+
 
 //----------------------------------------------------
-app.post("/login", Middleware.authJWT, controller.getUser);
-app.post("/register", controller.createUser1);
-app.get("/about", Middleware.authenticate, controller.requestSender);
+router.post("/register", controller.createUser1);
+router.post("/login", Middleware.authJWT, controller.getUser);
+router.get("/about", Middleware.authenticate, controller.requestSender);
 // app.get("/fetchName",controller.getUserbyQuery)
 // app.get("/companyAccess", controller.getCompany  )
 // --------------------------------------------------
 //  
 
 
-mongoose.set('strictQuery', true)
-mongoose.connect(process.env.DB).then(() => {
-  console.log("Mongo Connected");
-}).catch((err) => {
-  console.log("Not connected, try later");
-})
-
-
-
-//
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running at ${PORT}..`);
-});
-module.exports = app;
+module.exports = router
